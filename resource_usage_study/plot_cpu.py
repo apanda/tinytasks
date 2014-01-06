02dbeb7e4e0186d16b35d4b1bfd1fee5ac187fee
Kay Ousterhout <kayousterhout@gmail.com>
2014-01-06 12:57:34 -0800

    Added IO data

--- resource_usage_study/plot_cpu.py
+++ resource_usage_study/plot_cpu.py
@@ -12,8 +12,8 @@ def write_output_data(filename, data, earliest_time):
   f.close()
 
 def main(argv):
-  file_prefix = argv[0]
-  print "Parsing logs for %s" % file_prefix
+  file_prefix = argv[0].strip("/")
+  print "Parsing logs in directory %s" % file_prefix
 
   # List of tuples, where the first item is the time and the second
   # item is 1 if a task started at that time and -1 if a task
@@ -25,7 +25,13 @@ def main(argv):
   sys_cpu_usage = []
   total_cpu_usage = []
 
-  results_file = open("%s.log" % file_prefix)
+  # Time, IO rate tuples
+  rchar = []
+  rbytes = []
+  wchar = []
+  wbytes = []
+
+  results_file = open("%s/%s.log" % (file_prefix, file_prefix))
   for line in results_file:
     if line.find("Task run") != -1:
       items = line.split(" ")
@@ -34,15 +40,26 @@ def main(argv):
       end_time = int(items[9][:-1])
       task_events.append((end_time, -1))
     elif line.find("CPU utilization") != -1:
-      items=  line.split(" ")
+      items = line.split(" ")
       time = int(items[4])
       user_cpu_usage.append((time, items[8]))
       sys_cpu_usage.append((time, items[10]))
-      total_cpu_usage.append((time, items[12]))
+      total_cpu_usage.append((time, items[12][:-1]))
+    elif line.find("rchar") != -1:
+      items = line.split(" ")
+      time = int(items[4])
+      BYTES_PER_MB = 1048576
+      rchar.append((time, float(items[7]) / BYTES_PER_MB))
+      rbytes.append((time, float(items[13]) / BYTES_PER_MB))
+      wchar.append((time, float(items[10]) / BYTES_PER_MB))
+      wbytes.append((time, float(items[16][:-1]) / BYTES_PER_MB))
+    elif line.find("trans rates") != -1:
+      items = line.strip("\n").split(" ")
+      time = int(items[4])
 
   # Output file with number of running tasks.
   task_events.sort(key = lambda x: x[0])
-  running_tasks_filename = "%s.running_tasks" % file_prefix
+  running_tasks_filename = "%s/running_tasks" % file_prefix
   running_tasks_file = open(running_tasks_filename, "w")
   running_tasks = 0
   earliest_time = int(task_events[0][0])
@@ -52,33 +69,55 @@ def main(argv):
     running_tasks += event
     running_tasks_file.write("%s\t%s\n" % (time - earliest_time, running_tasks))
 
-  # Output CPU usage files.
-  user_cpu_filename = "%s.user_cpu" % file_prefix
+  # Output CPU usage data.
+  user_cpu_filename = "%s/user_cpu" % file_prefix
   write_output_data(user_cpu_filename, user_cpu_usage, earliest_time)
-  sys_cpu_filename = "%s.sys_cpu" % file_prefix
+  sys_cpu_filename = "%s/sys_cpu" % file_prefix
   write_output_data(sys_cpu_filename, sys_cpu_usage, earliest_time)
-  total_cpu_filename = "%s.total_cpu" % file_prefix
+  total_cpu_filename = "%s/total_cpu" % file_prefix
   write_output_data(total_cpu_filename, total_cpu_usage, earliest_time)
 
+  # Output IO usage data.
+  rchar_filename = "%s/rchar" % file_prefix
+  write_output_data(rchar_filename, rchar, earliest_time)
+  rbytes_filename = "%s/rbytes" % file_prefix
+  write_output_data(rbytes_filename, rbytes, earliest_time)
+  wchar_filename = "%s/wchar" % file_prefix
+  write_output_data(wchar_filename, wchar, earliest_time)
+  wbytes_filename = "%s/wbytes" % file_prefix
+  write_output_data(wbytes_filename, wbytes, earliest_time)
 
   # Warning: this graph ends up looking like a comb -- since the time in between task launches is
   # short relative to task runtime.
-  running_tasks_plot_file = open("%s_running_tasks_cpu.gp" % file_prefix, "w")
+  running_tasks_plot_file = open("%s/running_tasks_cpu.gp" % file_prefix, "w")
+  running_tasks_plot_file.write(
+    "# Must be plotted from the parent directory for links to work correctly\n")
   write_template(running_tasks_plot_file)
-  running_tasks_plot_file.write("set xlabel \"Time\"\n")
-  running_tasks_plot_file.write("set output \"%s_running_tasks_cpu.pdf\"\n\n" % file_prefix)
+  running_tasks_plot_file.write("set xlabel \"Time (ms)\"\n")
+  running_tasks_plot_file.write("set xrange [0:10000]\n")
+  running_tasks_plot_file.write("set y2tics\n")
+  running_tasks_plot_file.write("set y2label \"MB\"\n")
+  running_tasks_plot_file.write("set output \"%s/running_tasks_cpu.pdf\"\n\n" % file_prefix)
   running_tasks_plot_file.write(
     ("plot \"%s\" using 1:2 w l ls 1 title \"Running tasks\" axes x1y1,\\\n" %
       running_tasks_filename))
   running_tasks_plot_file.write(
-    ("\"%s\" using 1:2 w l ls 2 title \"User CPU\" axes x1y2,\\\n" %
+    ("\"%s\" using 1:2 w l ls 2 title \"User CPU\" axes x1y1,\\\n" %
       user_cpu_filename))
   running_tasks_plot_file.write(
-    ("\"%s\" using 1:2 w l ls 3 title \"System CPU\" axes x1y2,\\\n" %
+    ("\"%s\" using 1:2 w l ls 3 title \"System CPU\" axes x1y1,\\\n" %
       sys_cpu_filename))
   running_tasks_plot_file.write(
-    ("\"%s\" using 1:2 w l ls 4 title \"Total CPU\" axes x1y2\n" %
+    ("\"%s\" using 1:2 w l ls 4 title \"Total CPU\" axes x1y1,\\\n" %
       total_cpu_filename))
+  running_tasks_plot_file.write(
+    "\"%s\" using 1:2 w l ls 5 title \"rchar\" axes x1y2,\\\n" % rchar_filename)
+  running_tasks_plot_file.write(
+    "\"%s\" using 1:2 w l ls 6 title \"wchar\" axes x1y2,\\\n" % wchar_filename)
+  running_tasks_plot_file.write(
+    "\"%s\" using 1:2 w l ls 7 title \"rbytes\" axes x1y2,\\\n" % rbytes_filename)
+  running_tasks_plot_file.write(
+    "\"%s\" using 1:2 w l ls 8 title \"wbytes\" axes x1y2\n" % wbytes_filename)
 
 if __name__ == "__main__":
   main(sys.argv[1:])
